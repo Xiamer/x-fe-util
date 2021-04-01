a929ab544bae7934ef1721bd714ffc5e11bf5dee
xiamer_10 <5032651@qq.com>
2021-04-01 20:12:19 +0800

    feat: export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: xiaoguang_10@qq.com
  * @LastEditors: xiaoguang_10@qq.com
  * @Date: 2020-12-16 17:19:15
- * @LastEditTime: 2021-04-01 20:01:02
+ * @LastEditTime: 2021-04-01 20:10:46
  */
 
 import * as browser from './lib/browser'; // 浏览器相关
@@ -11,13 +11,11 @@ import * as design from './lib/design'; // 设计模式相关
 import * as pure from './lib/pure'; // 纯函数
 import * as idCard from './lib/id-card' // 身份证
 
-const o = {
+export default {
   ...browser,
   ...dom,
   ...design,
   ...pure,
   ...idCard
 }
-
-export default o
 
